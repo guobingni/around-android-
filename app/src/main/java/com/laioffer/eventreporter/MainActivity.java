86ca5f1785491784a247a4fab7ae9e50e73ab2a1
guobingni <guobingni@163.com>
2017-09-01 12:54:30 +0800

    First commit, activity and fragment communication

---
+++ app/src/main/java/com/laioffer/eventreporter/MainActivity.java
@@ -0,0 +1,84 @@
+package com.laioffer.eventreporter;
+
+import android.content.res.Configuration;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity implements EventFragment.OnItemSelectListener{
+    private EventFragment mListFragment;
+    private CommentFragment mGridFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //add list view
+        mListFragment = new EventFragment();
+        getSupportFragmentManager().beginTransaction().add(R.id.event_container, mListFragment).commit();
+
+
+        //add Gridview
+        if (isTablet()) {
+            mGridFragment = new CommentFragment();
+            getSupportFragmentManager().beginTransaction().add(R.id.comment_container, mGridFragment).commit();
+        }
+    }
+
+    private boolean isTablet() {
+        return (getApplicationContext().getResources().getConfiguration().screenLayout &
+                Configuration.SCREENLAYOUT_SIZE_MASK) >=
+                Configuration.SCREENLAYOUT_SIZE_LARGE;
+    }
+
+    /**
+     * A dummy function to get fake event names.
+     *
+     * @return an array of fake event names.
+     */
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e("Life cycle test", "We are at onStart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e("Life cycle test", "We are at onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e("Life cycle test", "We are at onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e("Life cycle test", "We are at onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e("Life cycle test", "We are at onDestroy()");
+    }
+
+    @Override
+    public void onItemSelected(int position) {
+        mGridFragment.onItemSelected(position);
+    }
+
+}
